850ee8012e77c1ffea046b88506dc759fb9f9655
my <irizaj07@gmail.com>
2020-06-22 05:12:21 +0100

    add examples

---
+++ models/DecisionTree.py
@@ -0,0 +1,22 @@
+class DecisionTree:
+    def __init__(self, X, Y):
+        pass
+
+
+    def fit(self):
+        pass
+
+
+    def predict(self, X_test):
+        pass
+
+    
+    def _calc_information_gain(self):
+        pass
+
+    def _calc_entropy(self):
+        pass
+
+    def _calc_gain(self):
+        pass
+
